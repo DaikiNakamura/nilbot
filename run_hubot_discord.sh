c1382653ba0f1e07713c609e8146d9b1bea87cbc
Daiki Nakamura <d.nakamura0717@gmail.com>
2017-07-19 13:50:43 +0900

    Update run_hubot_discord.sh

--- run_hubot_discord.sh
+++ run_hubot_discord.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-export REDIS_URL=redis://127.0.0.1:6379/hubot
-
-export HUBOT_DISCORD_TOKEN=`cat HUBOT_DISCORD_TOKEN`
+REDIS_URL=redis://127.0.0.1:6379/hubot
+HUBOT_DISCORD_TOKEN=`cat HUBOT_DISCORD_TOKEN`
 
 bin/hubot -a discord
